c462910dcbb0f95263536fabc2dd3093c72bfc37
Ziad Alzarka <ziadalzarka@gmail.com>
2020-11-26 15:20:36 +0200

    migrate gulp

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const fallback = require('express-history-api-fallback');
+const express = require("express");
+const fallback = require("express-history-api-fallback");
 
-const config = require('./src/config.js');
-const logger = require('./src/logger.js');
+const config = require("./src/config.js");
+const logger = require("./src/logger.js");
 
 const app = express();
-app.enable('trust proxy');
-app.use(logger.log4js.connectLogger(logger.getLogger('express'), {level: 'auto'}));
-const root = __dirname + config.app.front_path;
-app.use(express.static(root));
+app.enable("trust proxy");
+app.use(
+  logger.log4js.connectLogger(logger.getLogger("express"), { level: "auto" })
+);
 
-if (process.env.PORT)
-  config.app.port = process.env.PORT;
+if (process.env.PORT) config.app.port = process.env.PORT;
 app.listen(config.app.port, function () {
   logger.info(`${config.app.name} listening on port ${config.app.port}!`);
 });
 
 Promise.resolve()
   .then(() => {
-    app.use('/api', require('./api/index.js'));
-    logger.info('REST API attached to `/api` route');
-    app.use(fallback('index.html', { root }));
+    app.use("/api", require("./api/index.js"));
+    logger.info("REST API attached to `/api` route");
   })
-  .catch(err => {
+  .catch((err) => {
     logger.fatal(err.message);
     logger.error(err);
     db().close();
     process.exit(1);
-  })
+  });
